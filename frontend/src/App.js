c5df63b21f0684eb13d2d28913ae67482e165c7c
Kavin Babu <86172980+Kbcoder69@users.noreply.github.com>
2023-06-13 15:13:04 +0500

    Fixed npm start error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Xahir was here !!!!
+          DigiSchool
         </a>
       </header>
     </div>
