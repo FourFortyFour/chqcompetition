30d51c5bd319a03e646ef3f4f6e7098a43228e6d
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-06-14 07:09:30 +0400

    completed the basic information part of the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-
-
+import MyGrid from './components/BasicInfo';
+import VocabularyList from './components/VocabularyList';
 function App() {
   const [fields, setFields] = useState({})
   const [success, setSuccess] = useState(false)
   const [inputValue, setInputValue] = useState("")
+
+  const gridData = {
+    'lesson-title': 'Sample Lesson',
+    'subject': 'Science',
+    'teacher': 'John Doe',
+    'date': '2023-06-12',
+    'grade': '5th Grade',
+    'duration': '1 hour'
+  };
 
   const handleInputChange = (onQuerySend) => {
     setInputValue(onQuerySend.target.value)
@@ -46,10 +55,13 @@ function App() {
           <button type='submit'>Generate lesson plan</button>
         </form> : null}
         <div>{success ?
-          <h3>{fields["lesson-title"]}</h3>
+          <h3>{fields["key-vocabulary"]}</h3>
           :
           <h3>LOADING</h3>}</div>
       </header>
+      <MyGrid data={fields}></MyGrid>
+      <VocabularyList words={fields["key-vocabulary"].split(",")}></VocabularyList>
+      <VocabularyList words={fields["supporting-materials"]}></VocabularyList>
     </div>
   );
 }
--- frontend/src/components/BasicInfo.js
+++ frontend/src/components/BasicInfo.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './BasicInfo.css'
 
-const MyGrid = () => {
+const MyGrid = ({data}) => {
   return (
     <div className="grid-container">
-      <div id="lesson-title" className="grid-item">Lesson Title</div>
-      <div id="subject" className="grid-item">Subject</div>
-      <div id="teacher" className="grid-item">Teacher</div>
-      <div id="date" className="grid-item">Date</div>
-      <div id="grade" className="grid-item">Grade</div>
-      <div id="duration" className="grid-item">Duration</div>
+      <div id="lesson-title" className="grid-item">Lesson Title : {data["lesson-title"]}</div>
+      <div id="subject" className="grid-item">Subject : {data["subject"]}</div>
+      <div id="teacher" className="grid-item">Teacher : {data["teacher-name"]}</div>
+      <div id="date" className="grid-item">Date : {data["date"]}</div>
+      <div id="grade" className="grid-item">Grade : {data["grade"]}</div>
+      <div id="duration" className="grid-item">Duration : {data["duration"]}</div>
     </div>
   );
 }
