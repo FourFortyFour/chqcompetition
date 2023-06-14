3f031e5b16e9052b015eaa23dd6a3ac79ecdb7dd
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-06-14 06:36:35 +0400

    base web app working, bug occurance reduced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 
 
 function App() {
-  const [getMessage, setGetMessage] = useState({})
+  const [fields, setFields] = useState({})
+  const [success, setSuccess] = useState(false)
   const [inputValue, setInputValue] = useState("")
 
   const handleInputChange = (onQuerySend) => {
@@ -16,32 +17,36 @@ function App() {
     axios.post('http://localhost:5000/querygpt', { query: inputValue })
       .then(response => {
         // console.log("SUCCESS", response)
-        setGetMessage(response)
+        console.log(response)
+        setFields(response.data.response)
+        setSuccess(true)
         setInputValue("")
       }).catch(error => {
+        setSuccess(false)
         console.log(error)
       });
   };
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/querygpt').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/querygpt').then(response => {
+  //     console.log("SUCCESS", response)
+  //     setGetMessage(response)
+  //   }).catch(error => {
+  //     console.log(error)
+  //   })
 
-  }, [])
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Lesson plan generator</p>
-        <form onSubmit={handleSubmit}>
+        {!success ? <form onSubmit={handleSubmit}>
           <input type='text' value={inputValue} onChange={handleInputChange} />
           <button type='submit'>Generate lesson plan</button>
-        </form>
-        <div>{getMessage.status === 200 && getMessage.data ?
-          <h3>{getMessage.data.response}</h3>
+        </form> : null}
+        <div>{success ?
+          <h3>{fields["lesson-title"]}</h3>
           :
           <h3>LOADING</h3>}</div>
       </header>
